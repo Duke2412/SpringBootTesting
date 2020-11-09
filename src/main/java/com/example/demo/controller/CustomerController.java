9e95b587bc035a26c4a153d615532dd54c82a464
Duke2412 <66355961+Duke2412@users.noreply.github.com>
2020-11-09 14:25:36 +0700

    Initial commit

---
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Customer;
+import com.example.demo.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("customers")
+public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping
+    public ModelAndView showList() {
+        ModelAndView modelAndView = new ModelAndView("customers/list");
+        List<Customer> customers = customerService.findAll();
+        modelAndView.addObject("customers", customers);
+        return modelAndView;
+    }
+
+    @GetMapping("{id}")
+    public ModelAndView showInformation(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("customers/info");
+        Customer customer = customerService.findOne(id);
+        modelAndView.addObject("customer", customer);
+        return modelAndView;
+    }
+
+    @PostMapping
+    public String updateCustomer(Customer customer) {
+        customerService.save(customer);
+        return "redirect:/customers";
+    }
+}
